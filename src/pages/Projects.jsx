08415c7d5e0823d477053a33822422462a114fc4
Niki-desai <nikita.d10.nd@gmail.com>
2024-08-30 17:20:39 +0530

    changes3

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-
 import { CTA } from "../components";
 import { projects } from "../constants";
 import { arrow } from "../assets/icons";
@@ -15,11 +14,11 @@ const Projects = () => {
       </h1>
 
       <p className='text-slate-500 mt-2 leading-relaxed'>
-        I've embarked on numerous projects throughout the years, but these are
+        {/* I've embarked on numerous projects throughout the years, but these are
         the ones I hold closest to my heart. Many of them are open-source, so if
         you come across something that piques your interest, feel free to
         explore the codebase and contribute your ideas for further enhancements.
-        Your collaboration is highly valued!
+        Your collaboration is highly valued! */}
       </p>
 
       <div className='flex flex-wrap my-20 gap-16'>
